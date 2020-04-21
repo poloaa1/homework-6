79f996264753600a634ff19ba88839a18ad45b0f
poloaa1 <poloaa1@hotmail.com>
2020-04-20 17:36:56 -0700

    homework6

---
+++ main.js
@@ -0,0 +1,56 @@
+const api = {
+  key: "b4ea2bed4153c798555b4bc2134b6d44",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+function dateBuilder (d) {
+  let months = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
+"Friday", "Saturday"];
+
+let day = days[d.getDay()];
+let date = d.getDate();
+let month = months[d.getMonth()];
+let year = d.getFullYear();
+
+return `${day} ${date} ${month} ${year}`;
+}
+
+function setQuery(evt) {
+  if (evt.keyCode ==13) {
+    getResults(searchbox.value);
+    //console.log(searchbox.value);
+  }
+}
+
+function getResults (query) {
+  fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+  .then(weather => {
+    return weather.json();
+  }).then(displayResults);
+
+}
+
+
+
+function displayResults (weather) {
+  //console.log(weather);
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now); 
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°</span>`;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.weather[0].main;
+
+ 
+}
+
